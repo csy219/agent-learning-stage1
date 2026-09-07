66c819aa279cda7202abc1576ff9e1776ac32bfb
csy219 <3232162613@qq.com>
2026-09-07 11:23:03 +0800

    chat.py

--- chat.py
+++ chat.py
@@ -16,8 +16,12 @@ client=OpenAI(
 response=client.chat.completions.create(
     model="deepseek-v4-flash",
     messages=[
+        {"role":"system","content":"你是python助教,回答不超过50个字,回答简洁"},
         {"role":"user","content":"用一句话解释什么是虚拟环境"}
     ],
+   
+    temperature=0.1,
+    max_tokens=20,
 )
 print(response.choices[0].message.content)
 
